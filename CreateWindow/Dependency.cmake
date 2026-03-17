d89897ef4accbe256f72e4d65fdab1ea7c7e55dd
본ìsh <developersh0950@gmail.com>
2026-03-17 16:46:44 +0900

    cmakelist 오류 수정 windows창 생성

---
+++ CreateWindow/Dependency.cmake
@@ -0,0 +1,64 @@
+include(ExternalProject)
+
+set(DEP_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
+set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
+
+# 1. spdlog
+ExternalProject_Add(
+    dep_spdlog
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    CMAKE_ARGS 
+        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+        -DSPDLOG_BUILD_EXAMPLE=OFF
+        -DSPDLOG_BUILD_TESTS=OFF
+)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+
+if(WIN32)
+    # 디버그 모드일 때는 spdlogd, 릴리즈 모드일 때는 spdlog를 사용하도록 설정
+    set(DEP_LIBS ${DEP_LIBS} 
+        $<$<CONFIG:Debug>:spdlogd> 
+        $<$<NOT:$<CONFIG:Debug>>:spdlog>
+    )
+else()
+    set(DEP_LIBS ${DEP_LIBS} spdlog)
+endif()
+
+# 2. stb (디렉토리 생성 명령 분리)
+ExternalProject_Add(
+    dep_stb
+    GIT_REPOSITORY "https://github.com/nothings/stb"
+    GIT_TAG "master"
+    GIT_SHALLOW 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    TEST_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${DEP_INSTALL_DIR}/include/stb
+            COMMAND ${CMAKE_COMMAND} -E copy
+            ${PROJECT_BINARY_DIR}/dep_stb-prefix/src/dep_stb/stb_image.h
+            ${DEP_INSTALL_DIR}/include/stb/stb_image.h
+)
+set(DEP_LIST ${DEP_LIST} dep_stb)
+
+# 3. GLM (디렉토리 생성 명령 분리)
+ExternalProject_Add(
+    dep_glm
+    GIT_REPOSITORY "https://github.com/g-truc/glm"
+    GIT_TAG "1.0.1"
+    GIT_SHALLOW 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    TEST_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${DEP_INSTALL_DIR}/include/glm
+            COMMAND ${CMAKE_COMMAND} -E copy_directory
+            ${PROJECT_BINARY_DIR}/dep_glm-prefix/src/dep_glm/glm
+            ${DEP_INSTALL_DIR}/include/glm
+)
+set(DEP_LIST ${DEP_LIST} dep_glm)
+
+# Dependency.cmake 마지막 부분
+add_dependencies(${PROJECT_NAME} ${DEP_LIST})
--- CreateWindow/src/main.cpp
+++ CreateWindow/src/main.cpp
@@ -1,6 +1,54 @@
-#include <iostream>
+#include <windows.h>
+#include <spdlog/spdlog.h>
+
+// 윈도우 메시지 처리 함수 (창 닫기, 크기 조절 등)
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
 
 int main() {
-    std::cout << "Hello, CMake in VS Code!" << std::endl;
+    // 1. 현재 프로그램의 인스턴스 핸들을 직접 가져와야 합니다.
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    
+    // 2. 로그 확인 (창이 안 뜨면 터미널에 에러 로그가 찍히는지 보세요)
+    spdlog::info("Application Started");
+
+    const char* className = "DX11WindowClass";
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WindowProc, 0, 0, 
+                      hInstance, NULL, LoadCursor(NULL, IDC_ARROW), NULL, NULL, className, NULL };
+    
+    if (!RegisterClassEx(&wc)) {
+        spdlog::error("Window Class Registration Failed!");
+        return -1;
+    }
+
+    // 3. 창 생성 (창 크기가 0이 아닌지, 이름이 맞는지 확인)
+    HWND hWnd = CreateWindowEx(0, className, "Zenless Style PBR Pipeline", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, NULL, NULL, hInstance, NULL);
+
+    if (!hWnd) {
+        spdlog::error("Window Creation Failed! Error Code: {}", GetLastError());
+        return -1;
+    }
+
+    ShowWindow(hWnd, SW_SHOW); // SW_SHOW로 확실히 지정
+    UpdateWindow(hWnd);        // 즉시 그리도록 명령
+
+    spdlog::info("Window Displayed Successfully.");
+
+    // 4. 메시지 루프 (여기가 멈춰있어야 창이 유지됩니다)
+    MSG msg = { 0 };
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
     return 0;
 }
